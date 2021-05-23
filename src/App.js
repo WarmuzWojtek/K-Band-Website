1ba0279158908b5a8612b2003dd95c124bc519fd
WarmuzWojtek <wwarmuz@gmail.com>
2021-05-23 22:21:19 +0200

    Correct imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import NavBar from './Components/NavBar/NavBar.jsx'
+// import Carousel from './Components/Header/Header.jsx'
+
 
 function App() {
   return (
     <div >
-
+      <NavBar/>
+   
     </div>
   );
 }
