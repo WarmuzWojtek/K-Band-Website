69ca6a4f32869e4e28ee8c0e723ba5b8e7122d28
WarmuzWojtek <wwarmuz@gmail.com>
2021-06-23 22:22:02 +0200

    Add routing to mobile menu

--- src/Components/Menu/MobileMenu.jsx
+++ src/Components/Menu/MobileMenu.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { NavLink } from "react-router-dom";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,15 +19,60 @@ const useStyles = makeStyles((theme) => ({
       marginRight: theme.spacing(4),
     },
   },
+  item: {
+    textDecoration: 'none',
+    color: 'black',
+  }
 
 }));
 
 export default function MobileMenu() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <IconButton edge="start" color="inherit" aria-label="menu" >
-      <MenuIcon className={classes.menuIcon} />
-    </IconButton>
+    <>
+      <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
+        <MenuIcon className={classes.menuIcon} />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <NavLink to="/ludzie" className={classes.item} >
+          <MenuItem onClick={handleClose}>Ludzie</MenuItem>
+        </NavLink>
+        <NavLink to="/miejsca" className={classes.item}>
+          <MenuItem onClick={handleClose}>Miejsca</MenuItem>
+        </NavLink>
+        <NavLink to="/plyty" className={classes.item}>
+          <MenuItem onClick={handleClose}>Dyskografia</MenuItem>
+        </NavLink>
+        <NavLink to="/media" className={classes.item}>
+          <MenuItem onClick={handleClose}>Media</MenuItem>
+        </NavLink>
+        <NavLink to="/archiwalia" className={classes.item}>
+          <MenuItem onClick={handleClose}>Archiwalia</MenuItem>
+        </NavLink>
+        <NavLink to="/ksiega" className={classes.item}>
+          <MenuItem onClick={handleClose}>Księga Gości</MenuItem>
+        </NavLink>
+        <NavLink to="/kontakt" className={classes.item}>
+          <MenuItem onClick={handleClose}>Kontakt</MenuItem>
+        </NavLink>
+      </Menu>
+    </>
   )
 }
+
+
 
