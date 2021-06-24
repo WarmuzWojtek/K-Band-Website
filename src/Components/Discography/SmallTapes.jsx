151247ba282ad5b9781881fcf5422ed8c0a8423d
WarmuzWojtek <wwarmuz@gmail.com>
2021-06-24 13:50:14 +0200

    Add small to big recordings routing

--- src/Components/Discography/SmallTapes.jsx
+++ src/Components/Discography/SmallTapes.jsx
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import { NavLink } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,11 +19,16 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: 'right bottom',
     backgroundSize: '15%',
     backgroundRepeat: 'no-repeat',
-    backgroundColor: 'rgba(255,255,255,0.8)',
+    backgroundColor: 'rgba(255,255,255,0.6)',
   },
   image: {
     width: 200,
     height: 200,
+    '@media(max-width:600px)': {
+      width: 345,
+      height: 345,
+    },
+
   },
   img: {
     margin: 'auto',
@@ -33,42 +40,57 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
   },
+  typo: {
+    '@media(max-width:600px)': {
+      textAlign: 'center',
+    },
+  },
+  btn: {
+    '@media(max-width:600px)': {
+      display: 'flex',
+      margin: '0 auto',
+    },
+  },
+  link: {
+    textDecoration: 'none',
+  }
 }));
 
 export default function SmallCasetteCard({ title, year, label, medium, foto }) {
   const classes = useStyles();
+
+  const path = "/" + year;
 
   return (
     <div className={classes.root} >
       <Paper className={classes.paper} elevation={10} style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'images/punkpin.png'})`, }}>
         <Grid container spacing={2}>
           <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src={process.env.PUBLIC_URL + foto} />
-            </ButtonBase>
+            <NavLink to={path} >
+              <ButtonBase className={classes.image}>
+                <img className={classes.img} alt="complex" src={process.env.PUBLIC_URL + foto} />
+              </ButtonBase>
+            </NavLink>
           </Grid>
           <Grid item xs={12} sm container >
             <Grid item xs container direction="column" spacing={2} className={classes.content}>
               <Grid item xs>
-                <Typography gutterBottom variant="h6">
+                <Typography className={classes.typo} gutterBottom variant="h6">
                   {title}
                 </Typography>
-                <Typography variant="body2" gutterBottom>
+                <Typography className={classes.typo} variant="body2" gutterBottom>
                   {year + '  ' + label}
                 </Typography>
-                <Typography variant="body2" color="textSecondary">
+                <Typography className={classes.typo} variant="body2" color="textSecondary">
                   {medium}
                 </Typography>
               </Grid>
               <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Więcej
-                </Typography>
+                <NavLink to={path} className={classes.link}>
+                  <Button className={classes.btn} variant='contained' color='default'>Pokaż Więcej</Button>
+                </NavLink>
               </Grid>
             </Grid>
-            {/* <Grid item>
-              <Typography variant="subtitle1">$19.00</Typography>
-            </Grid> */}
           </Grid>
         </Grid>
       </Paper>
